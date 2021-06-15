63419e7b13da223433d9f99302a5ca4dbf49f14c
Julia Marie Christensen <iith030@upcorp.ad.uprr.com>
2021-06-15 15:57:21 -0500

    Sign Up Functionality

--- laughing-beyond/src/components/Signup.js
+++ laughing-beyond/src/components/Signup.js
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { Component } from "react"
+import axios from "axios"
 
-const Signup = () => {
-    return (
-        <div>
-            <h3>Sign Up</h3>
-            <form>
-                <input type='text' name='name' placeholder='enter your name' />
-                <input type='email' name='email' placeholder='enter your emil address' />
-                <input type='text' name='username' placeholder='create username' />
-                <input type='password' name='password' placeholder='create password' />
-                <input type='submit' value='Sign Up'/>
-            </form>
-        </div>
-    )
+class Signup extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            data: {
+                name:"",
+                email:"",
+                username:"",
+                password:"",
+
+            }
+        }
+    }
+
+    handleOnChange = (event) => {
+        this.setState(prevState => ({
+            data: {
+                ...prevState.data,
+                [event.target.name]: event.target.value
+            }
+        }))
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        axios.post("http://localhost:3001/user/signup", this.state.data)
+        .then(resp => {
+            console.log("user created!")
+            console.log(resp)
+            this.props.history.push(`/profile/${resp.data.user.id}`)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+
+    }
+    render() {
+        console.log(this.state.data)
+        return (
+            <div>
+                <h3>Sign Up</h3>
+                <form>
+                    <input onChange={this.handleOnChange} type='text' name='name' placeholder='enter your name' />
+                    <input onChange={this.handleOnChange} type='email' name='email' placeholder='enter your emil address' />
+                    <input onChange={this.handleOnChange} type='text' name='username' placeholder='create username' />
+                    <input onChange={this.handleOnChange} type='password' name='password' placeholder='create password' />
+                    <input type='submit' value='Sign Up'/>
+                </form>
+            </div>
+        )
+    }
+ 
 }
 
 export default Signup
