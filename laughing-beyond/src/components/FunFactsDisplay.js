ab4bbfbae8d7bfea625a63e82d48c51ec2d64c55
Julia Marie Christensen <iith030@upcorp.ad.uprr.com>
2021-06-17 12:25:01 -0500

    Drop Down

--- laughing-beyond/src/components/FunFactsDisplay.js
+++ laughing-beyond/src/components/FunFactsDisplay.js
@@ -6,22 +6,51 @@ class FunFactsDisplay extends Component  {
     constructor(props) {
         super(props)
         this.state = {
-            funFact: ''
+            funFact: '',
+            type: "trivia",
+            selectedOption: null
+
         }
     }
    
-    componentDidMount = () => {
-        axios.get('http://numbersapi.com/random?min=0&max=1000')
+    // componentDidMount = () => {
+    //     axios.get('http://numbersapi.com/random?min=0&max=1000')
+    //     .then(resp => {
+    //         console.log(resp.data)
+    //         this.setState({
+    //             funFact: resp.data,
+    //         })
+    //     })
+    // }
+    
+    fetchData = (inputValue) => {
+        axios.get(`http://numbersapi.com/#random/${inputValue}`)
         .then(resp => {
             console.log(resp.data)
-            this.setState({
-                funFact: resp.data,
-            })
         })
+    }
+
+    handleChange(event) {
+        event.persist()
+        this.setState(prevState=>({
+            ...prevState,
+
+          type: event.target.value
+            
+        }))
+      console.log(this.state.type)
+    }
+
+    handleSubmit(event) {
+        // alert('here are your options' + this.state.value);
+        event.preventDefault();
+        console.log(this.state.type)
+        this.fetchData()
     }
 
     render() {
         const funFact = this.state.funFact
+        console.log(this.state.type)
         return (
             <div id='funFacts'>
                 <h3>Fun Facts with Numbers</h3>
@@ -30,7 +59,7 @@ class FunFactsDisplay extends Component  {
                         <p className='funFact'>{funFact}</p>
                         <button><a onClick={() => {window.location.href="/funfacts"}}>New Fun Fact</a></button>
                     </div>
-                    <FunFactsSearch />
+                    <FunFactsSearch value={this.state.type} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
                 </div>
             </div>
         ) 
--- laughing-beyond/src/components/FunFactsSearch.js
+++ laughing-beyond/src/components/FunFactsSearch.js
@@ -1,25 +1,42 @@
 import React from 'react'
+import Select from 'react-select'
 
-const FunFactsSearch = () => {
-    return (
-        <div className='factsSearch'>
-            <form>
-                <label></label>
-                <input type='dropdown'
-                    // <option>trivia</option>
-                    // <option>math</option>
-                    // <option>date</option>
-                    // <option>year</option>
-                />
-                <label></label>
-                <input type='integer' name='value' />
-                <label></label>
-                <input type="date" name='date'/>
-                <input type="" name='year'/>
-            </form>
+const searchType = [
+    {label: "Trivia", value: "trivia"},
+    {label: "Math", value: "math"},
+    {label: "Date", value: "date"},
+    {label: "Year", value: "year"}
+]
 
-        </div>
-    )
-}
+const FunFactsSearch = (props) => {
+    
+    
+        return (
+            <div className='factsSearch'>
+                <form onSubmit={props.handleSubmit}>
+                {/* <Select options={searchType} /> */}
+                    {/* <label> */}
+                      
+                    {/* </label>  */}
+                  
+{/*                     
+                    <label></label>
+                    <input type='integer' name='value' />
+                    <label></label>
+                    <input type="date" name='date'/>
+                    <input type="" name='year'/> */}
+                </form>
+                <select value={props.type} name="type" type="dropdown" onChange={props.handleChange}>
+                            
+                            <option value="trivia">Trivia</option>
+                            <option value="math">Math</option>
+                            <option value="date">Date</option>
+                            <option value="year">Year</option>
+                        </select>
+                        {/* <input type="submit" value="Submit" /> */}
+            </div>
+        )
+    }
+
 
 export default FunFactsSearch
